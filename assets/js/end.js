e67390b59f7f1737a9ca094a5d8d505fbc7b6483
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2021-07-12 18:17:46 +0000

    Add basic styling to leaderboard page

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,13 +1,13 @@
-const username = document.querySelector('#username')
-const saveScoreBtn = document.querySelector('#saveScoreButton')
-const finalScore = document.querySelector('#finalScore')
-const mostRecentScore = localStorage.getItem('mostRecentScore')
+const username = document.querySelector('#username');
+const saveScoreBtn = document.querySelector('#saveScoreButton');
+const finalScore = document.querySelector('#finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5
 
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
