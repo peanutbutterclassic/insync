4b6fdf3a95e0da48d4fb340cbd6890547afad84e
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2021-07-13 00:47:35 +0000

    Change p to button in order to comply with best practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -341,7 +341,7 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if (availableQuestions.length === 0 || questionCounter === MAX_QUESTIONS) {
         localStorage.setItem("mostRecentScore", score)
         
         return window.location.assign("/end.html")
@@ -448,3 +448,4 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
